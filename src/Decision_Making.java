d76733891f6817942836388916c887d2477afa26
NIPHAM53 <nipham53@gmail.com>
2022-04-04 03:20:22 -0400

    Changed if to else if

--- src/Decision_Making.java
+++ src/Decision_Making.java
@@ -14,11 +14,11 @@ public class Decision_Making {
 		int numInput = scnr.nextInt();
 		if (numInput % 2 == 0) {
 			System.out.println(input + ", you chose " + numInput + " and it is odd");
-		}if (numInput >= 2 && numInput % 2 == 1 && numInput <= 25) {
+		}else if (numInput >= 2 && numInput % 2 == 1 && numInput <= 25) {
 			System.out.println(input + ".The number you chose, " + numInput + ",is even and less than 25");	
-		}if (numInput >= 26 && numInput <= 60 && numInput % 2 ==1) {
+		}else if (numInput >= 26 && numInput <= 60 && numInput % 2 ==1) {
 			System.out.println(input + ".The number you chose is even and between 26 to 60.");
-		}if (numInput >= 60 && numInput %2 == 1) {
+		}else if (numInput >= 60 && numInput %2 == 1) {
 			System.out.println(input + ", the number you chose is" + numInput + ". It is even." );
 		}
 		}
